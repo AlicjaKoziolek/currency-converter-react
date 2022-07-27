c85c8da3b9d0effbcb83fa8aa14766a373ec6e1d
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-07-27 09:00:19 +0200

    added interactions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import Footer from "./Footer";
 import Form from "./Form";
 import Header from "./Header";
+import { useState } from "react";
+import { currencies } from "./Form/index.js";
 
 function App() {
+  const [result, setResult] = useState();
+
+  function calculateResult(currency, amount) {
+    const rate = currencies.find(({ money }) => money === currency).rate;
+    setResult({
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
   return (
     <body className="body">
       <main className="calculator">
         <Header title={"Kalkulator walutowy"} />
-        <Form />
+        <Form 
+        result={result} 
+        calculateResult={calculateResult}
+         />
         <p className="calculator__paragraph">
-          Kurs walut wynosi: <strong>N/A</strong>
+          Kurs walut wynosi:{" "}
+          <strong>
+            {result.targetAmount.toFixed(2)}
+            {result.currency}
+          </strong>
         </p>
       </main>
       <Footer footerContent={"Projekt autorstwa Alicji Koziołek"} />
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,35 +1,73 @@
 import "./style.css";
+import { useState } from "react";
 
-const Form = () => (
+export const currencies = [
+  {
+    money: "USD",
+    name: "Dolar amerykański",
+    rate: 4.68,
+  },
+  {
+    money: "Eur",
+    name: "Euro",
+    rate: 4.75,
+  },
+];
+
+export const Form = ({ calculateResult, result }) => {
+  const [currency, setCurrency] = useState([0].money);
+  const [amount, setAmount] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
+
+  return (
     <form>
-        <fieldset className="calculator__fieldset">
-            <legend className="calculator__legend">Przelicz walutę:</legend>
-            <p>
-                Pola oznaczone * są wymagane
-            </p>
-            <p>
-                <label>
-                    <span>Podaj kwotę*: </span><input name="value" placeholder="PLN" type="number"
-                        min="1" required/>
+      <fieldset className="calculator__fieldset">
+        <legend className="calculator__legend">Przelicz walutę:</legend>
+        <p>Pola oznaczone * są wymagane</p>
+        <p>
+          <label>
+            <span>Podaj kwotę*: </span>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              name="value"
+              placeholder="PLN"
+              type="number"
+              min="1"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span>Przelicz na:</span>
+            <select
+              name="currency:"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.money} value={currency.money}>
+                  {currency.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
 
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span>Przelicz na:</span>
-                    <select name="currency:">
-                        <option type="number" value="eur" selected>euro (EUR)</option>
-                        <option type="number" value="usd">dolar amerykański (USD)</option>
-                    </select>
-                </label>
-            </p>
-
-            <section><button className="calculator__button">Sprawdź kurs</button>
-                <button type="reset" className="calculator__button">Wyczyść</button>
-            </section>
-
-        </fieldset>
+        <section>
+          <button className="calculator__button">Sprawdź kurs</button>
+          <button type="reset" className="calculator__button">
+            Wyczyść
+          </button>
+        </section>
+      </fieldset>
     </form>
-);
+  );
+};
 
 export default Form;
