f5edf19e745f77463cdc84deb90a0e872e32fad9
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-03 07:47:59 +0200

    changed <body> at <div> and changed view for component Form

--- src/App.js
+++ src/App.js
@@ -5,34 +5,28 @@ import { useState } from "react";
 import { currencies } from "./Form/index.js";
 
 function App() {
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState();
 
-  function calculateResult(currency, amount) {
+  const calculateResult = (currency, amount) => {
     const rate = currencies.find(({ money }) => money === currency).rate;
+
     setResult({
+      enteredAmount: +amount,
       targetAmount: amount / rate,
       currency,
     });
-  }
+  };
 
   return (
-    <body className="body">
-      <main className="calculator">
-        <Header title={"Kalkulator walutowy"} />
-        <Form 
-        result={result} 
-        calculateResult={calculateResult}
-         />
-        <p className="calculator__paragraph">
-          Kurs walut wynosi:{" "}
-          <strong>
-            {result && result.targetAmount.toFixed(2)}
-            {result && result.currency}
-          </strong>
-        </p>
-      </main>
-      <Footer footerContent={"Projekt autorstwa Alicji Koziołek"} />
-    </body>
+      <div className="container">
+        <main className="calculator">
+          <Header title={"Kalkulator walutowy"} />
+          <Form 
+          result={result} 
+          calculateResult={calculateResult} />
+        </main>
+        <Footer footerContent={"Projekt autorstwa Alicji Koziołek"} />
+      </div>
   );
 }
 
