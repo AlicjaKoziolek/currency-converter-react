ef2f4d0fabb766cd471722370c560e6260052930
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-03 07:32:34 +0200

    formatted code

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,7 +1,7 @@
-import "./style.css"
+import "./style.css";
 
-const Footer = ({footerContent}) => (
-    <footer className="body__footer">{footerContent}</footer>
+const Footer = ({ footerContent }) => (
+  <footer className="body__footer">{footerContent}</footer>
 );
 
 export default Footer;
