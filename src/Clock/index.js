5ff3407dd7a10ad561017199e5dd8bd32dfe7c36
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-18 08:56:22 +0200

    created function formatDate in component Clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,21 +1,23 @@
 import { useDate } from "./useDate.js";
 import "./style.css";
 
+const formatDate = (date) =>
+  date.toLocalString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
 const Clock = () => {
   const date = useDate();
 
   return (
-    <div className="clock">
-      Dzisiaj jest
-      {" "}
-      {date.toLocalString(undefined, {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-      })}
+  <div className="clock">
+    Dzisiaj jest 
+    {formatDate(date)}
     </div>
   );
 };
