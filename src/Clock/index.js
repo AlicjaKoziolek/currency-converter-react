008cff47d804178545b603e45529fcc467874d2c
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-18 13:36:17 +0200

    corrected typed error

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,23 +1,21 @@
 import { useDate } from "./useDate.js";
 import "./style.css";
 
-const formatDate = (date) =>
-  date.toLocalString(undefined, {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
-
 const Clock = () => {
   const date = useDate();
 
   return (
   <div className="clock">
     Dzisiaj jest 
-    {formatDate(date)}
+    {" "}
+    {date.toLocaleString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  })}
     </div>
   );
 };
