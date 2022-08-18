aeb68c2fd3d6e7511879b58ad7eb63473a3aff26
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-18 08:41:16 +0200

    created component Clock

---
+++ src/Clock/index.js
@@ -0,0 +1,23 @@
+import { useDate } from "./useDate.js";
+import "./style.css";
+
+const Clock = () => {
+  const date = useDate();
+
+  return (
+    <div className="clock">
+      Dzisiaj jest
+      {" "}
+      {date.toLocalString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+      })}
+    </div>
+  );
+};
+
+export default Clock;
