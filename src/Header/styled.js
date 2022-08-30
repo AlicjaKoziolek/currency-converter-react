c811ef00949a3a67af41731f11f314f4574624bc
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-30 16:16:24 +0200

    added styled-components for Header component

---
+++ src/Header/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainHeader = styled.header`
+  background-color: rgb(3, 184, 184);
+  color: rgb(26, 26, 95);
+  font-family: "Josefin Sans", sans-serif;
+  text-align: center;
+  padding: 5px;
+  border-radius: 10px;
+  font-weight: bolder;
+  text-shadow: 1px 2px 3px black;
+  margin-bottom: 20px;
+`;
