e7b79a228ecac221eb31870ec89e38a1d9b67ef1
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-08-31 18:27:26 +0200

    added GlobalStyle for the entire application

---
+++ src/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./background.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+   html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    .container {
+        display: flex;
+        overflow-x: auto;
+        background-image: url("${background}");
+        background-position: center;
+        background-size: cover;
+        min-height: 100vh;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .main {
+        background-color: cornsilk;
+    }
+`;
